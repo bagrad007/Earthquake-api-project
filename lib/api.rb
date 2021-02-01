91870529970d1ed52e347c36dda451a2d73a899f
Luke Gill <bagrad007@gmail.com>
2021-02-01 10:38:32 -0500

    first commit

---
+++ lib/api.rb
@@ -0,0 +1,8 @@
+class Api
+  @@api_key = "CfLh8ssEDtxbhdlH1hLNn9vSOSP3z8ZGcUDOoCHu"
+
+  def self.get_report_by_zip(zip)
+    url = "https://api.aerisapi.com/earthquakes/"
+    response = HTTParty.get(url)
+  end
+end
---
+++ lib/cli.rb
@@ -0,0 +1,5 @@
+require "pry"
+
+require_relative("../lib/api")
+require_relative("../lib/cli")
+require_relative("../lib/location")
