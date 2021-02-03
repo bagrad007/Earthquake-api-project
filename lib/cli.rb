b74d26212ccfa6ed5dce183d994f3151479f7fc4
Luke Gill <bagrad007@gmail.com>
2021-02-03 11:12:40 -0500

    Changes formatting of output in info_options

--- lib/cli.rb
+++ lib/cli.rb
@@ -48,13 +48,19 @@ class Cli
       puts "An estimated 10,000 light type quakes happen per year."
       self.prompt_place_or_exit
     elsif input == 1 && location.mag.between?(5, 5.9)
-      puts "The magnitude is #{location.mag}. This type of quake is felt by most people. Can possibly cause broken plaster and chimneys to buildings. An estimated 1,000 moderate type quakes happen per year."
+      puts "The magnitude is #{location.mag}. This type of quake is felt by most people."
+      puts "Can possibly cause broken plaster and chimneys to buildings."
+      puts "An estimated 1,000 moderate type quakes happen per year."
       self.prompt_place_or_exit
     elsif input == 1 && location.mag.between?(6, 6.9)
-      puts "The magnitude is #{location.mag}. Damage will vary based on how well the construction of the building was made. Poorly built buildings will sustain considerable damage, while buildings with good design and construction will have negligible damage. An estimated 130 strong type quakes happen each year."
+      puts "The magnitude is #{location.mag}. Damage will vary based on how well the construction of the building was made."
+      puts "Poorly built buildings will sustain considerable damage, while buildings with good design and construction will have negligible damage."
+      puts "An estimated 130 strong type quakes happen each year."
       self.prompt_place_or_exit
     elsif input == 1 && location.mag.between?(7, 7.9)
-      puts "The magnitude is #{location.mag}. Damage will be considerable in specially designed structures, with well-designed frame structures thrown out of plumb. Buildings can sustain partial collapse and be shifted off foundations. An estimated 15 major type quakes happen per year."
+      puts "The magnitude is #{location.mag}. Damage will be considerable in specially designed structures, with well-designed frame structures thrown out of plumb."
+      puts "Buildings can sustain partial collapse and be shifted off foundations."
+      puts "An estimated 15 major type quakes happen per year."
       self.prompt_place_or_exit
     elsif input == 1 && location.mag.between?(8, 8.4)
       puts "The magnitude is #{location.mag}. Some well-built wooden structures destroyed; most masonry and frame stuctures destroyed along with the foundations. Rails will be bent. It's estimated that only 1 of these great type quakes happen every year."
