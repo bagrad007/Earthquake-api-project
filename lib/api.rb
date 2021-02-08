e4ec777a5f797616ddeda4031fbbea8752b508f4
Luke Gill <bagrad007@gmail.com>
2021-02-08 14:36:26 -0500

    Changes code for api

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,6 @@
 class Api
-  @@api_key = "CfLh8ssEDtxbhdlH1hLNn9vSOSP3z8ZGcUDOoCHu"
   def self.get_report_by_place(place)
-    url = "https://api.aerisapi.com/earthquakes/closest?p=#{place}&radius=3000miles&client_id=TPgjRDTtKBBQd4Q2fLI3c&client_secret=#{@@api_key}"
+    url = "https://api.aerisapi.com/earthquakes/closest?p=#{place}&radius=3000miles&client_id=TPgjRDTtKBBQd4Q2fLI3c&client_secret=#{ENV["API_KEY"]}"
     response = HTTParty.get(url)
     if response["error"]
       return false
